28ba04188f3f951881ac6ac3886bc08669f13e84
famicucci <famicucci@gmail.com>
2022-05-31 18:05:54 -0300

    Correct update order detail

--- app/controllers/OrdenDetalleController.js
+++ app/controllers/OrdenDetalleController.js
@@ -15,6 +15,7 @@ exports.modificarDetalleOrden = async (req, res) => {
 		const invoice = await Factura.findOne({
 			where: { OrdenId: req.params.OrdenId },
 		});
+
 		if (invoice) {
 			await t.rollback();
 			res.status(400).send({
@@ -36,94 +37,110 @@ exports.modificarDetalleOrden = async (req, res) => {
 		});
 
 		// add current products quantitys to stock
-		for (x of currentProducts) {
-			if (x.PtoStockId)
-				await Stock.increment(
-					{ cantidad: x.cantidad },
-					{
-						transaction: t,
-						where: {
-							ProductoCodigo: x.ProductoCodigo,
-							PtoStockId: x.PtoStockId,
-						},
-					}
-				);
-		}
+		const restablishStock = async () => {
+			for (x of currentProducts) {
+				if (x.PtoStockId)
+					await Stock.increment(
+						{ cantidad: x.cantidad },
+						{
+							transaction: t,
+							where: {
+								ProductoCodigo: x.ProductoCodigo,
+								PtoStockId: x.PtoStockId,
+							},
+						}
+					);
+			}
+			return;
+		};
+		await restablishStock();
+		await t.commit();
 
-		// check if all products are available (if not do a rollback)
-		const productStocks = await Stock.findAll();
-		for (x of req.body) {
-			for (y of productStocks) {
-				if (
-					x.ProductoCodigo === y.ProductoCodigo &&
-					x.PtoStockId === y.PtoStockId
-				) {
-					if (y.cantidad - x.cantidad < 0) {
-						await t.rollback();
-						res.status(400).send({
-							msg: `El producto ${x.ProductoCodigo} no tiene stock suficiente`,
-							severity: 'error',
-						});
-						return;
+		const t2 = await sequelize.transaction();
+		try {
+			// check if all products are available (if not do a rollback)
+			const productStocks = await Stock.findAll();
+			for (x of req.body) {
+				for (y of productStocks) {
+					if (
+						x.ProductoCodigo === y.ProductoCodigo &&
+						x.PtoStockId === y.PtoStockId
+					) {
+						if (y.cantidad - x.cantidad < 0) {
+							await t2.rollback();
+							res.status(400).send({
+								msg: `El producto ${x.ProductoCodigo} no tiene stock suficiente`,
+								severity: 'error',
+							});
+							return;
+						}
 					}
 				}
 			}
-		}
 
-		// create new order detail
-		await OrdenDetalle.bulkCreate(req.body, {
-			transaction: t,
-		});
+			// create new order detail
+			await OrdenDetalle.bulkCreate(req.body, {
+				transaction: t2,
+			});
 
-		// remove quantitys from stock
-		for (x of req.body) {
-			if (x.PtoStockId)
-				await Stock.decrement(
-					{ cantidad: x.cantidad },
-					{
-						transaction: t,
-						where: {
-							ProductoCodigo: x.ProductoCodigo,
-							PtoStockId: x.PtoStockId,
-						},
-					}
-				);
-		}
-
-		// finally create corresponding rows in stock movement
-		let currentProductsToNegative = [];
-		currentProducts.forEach((n) => {
-			n.cantidad = -n.cantidad;
-			currentProductsToNegative.push(n);
-		});
-		const mergeToMovements = [...currentProductsToNegative, ...req.body];
-
-		let movements = {};
-		mergeToMovements.forEach((x) => {
-			if (x.PtoStockId) {
-				const id = `${x.ProductoCodigo}${x.PtoStockId}`;
-				movements[id] = movements[id] ?? {
-					cantidad: 0,
-					motivo: 'M. Venta',
-					ProductoCodigo: x.ProductoCodigo,
-					PtoStockId: x.PtoStockId,
-					UsuarioId: req.usuarioId,
-				};
-				movements[id]['cantidad'] += x.cantidad;
+			// remove quantitys from stock
+			for (x of req.body) {
+				if (x.PtoStockId)
+					await Stock.decrement(
+						{ cantidad: x.cantidad },
+						{
+							transaction: t2,
+							where: {
+								ProductoCodigo: x.ProductoCodigo,
+								PtoStockId: x.PtoStockId,
+							},
+						}
+					);
 			}
-		});
 
-		await MovimientoStock.bulkCreate(
-			Object.values(movements).filter((x) => x.cantidad !== 0),
-			{
-				transaction: t,
-			}
-		);
+			// finally create corresponding rows in stock movement
+			let currentProductsToNegative = [];
+			currentProducts.forEach((n) => {
+				n.cantidad = -n.cantidad;
+				currentProductsToNegative.push(n);
+			});
+			const mergeToMovements = [...currentProductsToNegative, ...req.body];
 
-		await t.commit();
-		res.json({ msg: 'Orden modificada', severity: 'success' });
+			let movements = {};
+			mergeToMovements.forEach((x) => {
+				if (x.PtoStockId) {
+					const id = `${x.ProductoCodigo}${x.PtoStockId}`;
+					movements[id] = movements[id] ?? {
+						cantidad: 0,
+						motivo: 'M. Venta',
+						ProductoCodigo: x.ProductoCodigo,
+						PtoStockId: x.PtoStockId,
+						UsuarioId: req.usuarioId,
+					};
+					movements[id]['cantidad'] += x.cantidad;
+				}
+			});
+
+			await MovimientoStock.bulkCreate(
+				Object.values(movements).filter((x) => x.cantidad !== 0),
+				{
+					transaction: t2,
+				}
+			);
+
+			await t2.commit();
+			// res.json(productStocks);
+			res.json({ msg: 'Orden modificada', severity: 'success' });
+		} catch (error) {
+			// res.status(400).send('hola como está');
+			console.log(error);
+			res.status(400).json(error);
+			await t2.rollback();
+		}
 	} catch (error) {
+		// res.status(400).send('hola como está');
+		console.log(error);
+		res.status(400).json(error);
 		await t.rollback();
-		res.status(400).send(error);
 	}
 };
--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
 	username: process.env.DB_USERNAME || 'root',
 	password: process.env.DB_PASSWORD || '',
-	database: process.env.DB_DATABASE || 'sip-e2',
+	database: process.env.DB_DATABASE || 'sip-e-testing',
 	host: process.env.DB_HOST || 'localhost',
 	dialect: process.env.DB_DIALECT || 'mysql',
 
