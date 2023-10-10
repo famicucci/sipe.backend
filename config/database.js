939a471739bb178402dbdd67773d8bc3b62de0ad
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-10-10 15:18:50 -0300

    Change database config

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-	username: process.env.DB_USERNAME || 'root',
-	password: process.env.DB_PASSWORD || '',
-	database: process.env.DB_DATABASE || 'sip-e-testing',
-	host: process.env.DB_HOST || 'localhost',
-	dialect: process.env.DB_DIALECT || 'mysql',
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DATABASE || "sipe",
+  host: process.env.DB_HOST || "localhost",
+  dialect: process.env.DB_DIALECT || "mysql",
 
-	// configurar seeds
-	seederStorage: 'sequelize',
-	seederStorageName: 'seeds',
+  // configurar seeds
+  seederStorage: "sequelize",
+  seederStorageName: "seeds",
 
-	// Configurar migraciones
-	migrationStorage: 'sequelize',
-	migrationStorageName: 'migrations',
+  // Configurar migraciones
+  migrationStorage: "sequelize",
+  migrationStorageName: "migrations",
 };
