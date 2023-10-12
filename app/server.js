5389f94c434c32493a7f315dd363702f31dda3bb
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-10-12 16:18:45 -0300

    Create cookie with token

--- app/server.js
+++ app/server.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const { sequelize } = require('./models/index');
-const apiRouter = require('./routes/api');
-const cors = require('cors');
+require("dotenv").config();
+const { sequelize } = require("./models/index");
+const apiRouter = require("./routes/api");
+const cors = require("cors");
+var cookies = require("cookie-parser");
 
 // habilitar cors
-app.use(cors());
+app.use(cookies());
+app.use(
+  cors({
+    origin: process.env.ORIGIN_URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: false }));
@@ -15,13 +22,13 @@ app.use(express.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 4000;
 
 // rutas
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 // arrancar el servidor
 app.listen(PORT, () => {
-	console.log(`La aplicación ha arrancado en el puerto: ${PORT}`);
+  console.log(`La aplicación ha arrancado en el puerto: ${PORT}`);
 
-	// sequelize.sync({ force: false }).then(() => {
-	// 	console.log('Se ha establecido la conexión');
-	// });
+  // sequelize.sync({ force: false }).then(() => {
+  // 	console.log('Se ha establecido la conexión');
+  // });
 });
