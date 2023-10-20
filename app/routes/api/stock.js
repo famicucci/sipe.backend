1c04b9f736a36b917001d57473c576ee27cb3ee8
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-10-20 18:03:19 -0300

    Get stock with search and pagination

--- app/routes/api/stock.js
+++ app/routes/api/stock.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const StockController = require('../../controllers/StockController');
+const StockController = require("../../controllers/StockController");
 
-router.put('/', StockController.modificarStock);
-router.get('/total', StockController.traerStockTotal);
-router.get('/pto-stock', StockController.traerStockPtoStock);
-router.get('/producto/:codigo', StockController.traerStockProducto);
-router.get('/movimientos', StockController.traerMovimientos);
+router.put("/", StockController.modificarStock);
+router.get("/", StockController.getStock);
+router.get("/total", StockController.traerStockTotal);
+router.get("/pto-stock", StockController.traerStockPtoStock);
+router.get("/producto/:codigo", StockController.traerStockProducto);
+router.get("/movimientos", StockController.traerMovimientos);
 
 // traer los puntos de stock
-router.get('/ptos-stock', StockController.traerPtosStock);
+router.get("/ptos-stock", StockController.traerPtosStock);
 
 module.exports = router;
