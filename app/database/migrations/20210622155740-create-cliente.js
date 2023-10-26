4bbf6a2097e9237803abb845aac7e4d9daeabb0b
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-10-26 14:58:36 -0300

    Change datatype of email in order to accept 120 characters

--- app/database/migrations/20210622155740-create-cliente.js
+++ app/database/migrations/20210622155740-create-cliente.js
@@ -1,70 +1,70 @@
-'use strict';
+"use strict";
 module.exports = {
-	up: async (queryInterface, Sequelize) => {
-		await queryInterface.createTable('cliente', {
-			id: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-			},
-			nombre: {
-				type: Sequelize.STRING(30),
-				allowNull: false,
-			},
-			apellido: {
-				type: Sequelize.STRING(30),
-				allowNull: false,
-			},
-			observaciones: {
-				type: Sequelize.STRING(120),
-			},
-			instagram: {
-				type: Sequelize.STRING(30),
-			},
-			facebook: {
-				type: Sequelize.STRING(30),
-			},
-			celular: {
-				type: Sequelize.STRING(30),
-			},
-			email: {
-				type: Sequelize.STRING(30),
-				allowNull: false,
-			},
-			mascota: {
-				type: Sequelize.STRING(30),
-			},
-			tipo: {
-				type: Sequelize.STRING(15),
-				allowNull: false,
-			},
-			dni: {
-				type: Sequelize.STRING(15),
-			},
-			razonSocial: {
-				type: Sequelize.STRING(30),
-			},
-			condIva: {
-				type: Sequelize.STRING(30),
-				allowNull: false,
-			},
-			createdAt: {
-				type: Sequelize.DATE,
-				allowNull: false,
-			},
-			updatedAt: {
-				type: Sequelize.DATE,
-				allowNull: false,
-			},
-			EmpresaId: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-				references: { model: 'empresa', key: 'id' },
-			},
-		});
-	},
-	down: async (queryInterface, Sequelize) => {
-		await queryInterface.dropTable('cliente');
-	},
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("cliente", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nombre: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      apellido: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      observaciones: {
+        type: Sequelize.STRING(120),
+      },
+      instagram: {
+        type: Sequelize.STRING(30),
+      },
+      facebook: {
+        type: Sequelize.STRING(30),
+      },
+      celular: {
+        type: Sequelize.STRING(30),
+      },
+      email: {
+        type: Sequelize.STRING(120),
+        allowNull: false,
+      },
+      mascota: {
+        type: Sequelize.STRING(30),
+      },
+      tipo: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+      },
+      dni: {
+        type: Sequelize.STRING(15),
+      },
+      razonSocial: {
+        type: Sequelize.STRING(30),
+      },
+      condIva: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      EmpresaId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "empresa", key: "id" },
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("cliente");
+  },
 };
--- app/models/cliente.js
+++ app/models/cliente.js
@@ -1,51 +1,51 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-	class Cliente extends Model {
-		static associate(models) {
-			Cliente.belongsTo(models.Empresa, {
-				foreignKey: { allowNull: false },
-				onDelete: 'RESTRICT',
-				onUpdate: 'RESTRICT',
-			});
-			Cliente.hasMany(models.Direccion, {
-				as: 'direcciones',
-				foreignKey: { allowNull: false },
-				onDelete: 'CASCADE',
-				onUpdate: 'CASCADE',
-			});
-			Cliente.hasMany(models.Orden, {
-				foreignKey: { allowNull: false },
-				onDelete: 'RESTRICT',
-				onUpdate: 'RESTRICT',
-			});
-			Cliente.hasMany(models.Factura, {
-				foreignKey: { allowNull: false },
-				onDelete: 'NO ACTION',
-				onUpdate: 'NO ACTION',
-			});
-		}
-	}
-	Cliente.init(
-		{
-			nombre: { type: DataTypes.STRING(30), allowNull: false },
-			apellido: { type: DataTypes.STRING(30), allowNull: false },
-			observaciones: { type: DataTypes.STRING(120) },
-			instagram: { type: DataTypes.STRING(30) },
-			facebook: { type: DataTypes.STRING(30) },
-			celular: { type: DataTypes.STRING(30) },
-			email: { type: DataTypes.STRING(30), allowNull: false },
-			mascota: { type: DataTypes.STRING(30) },
-			tipo: { type: DataTypes.STRING(15), allowNull: false },
-			dni: { type: DataTypes.STRING(15) },
-			razonSocial: { type: DataTypes.STRING(30) },
-			condIva: { type: DataTypes.STRING(30), allowNull: false },
-		},
-		{
-			sequelize,
-			modelName: 'Cliente',
-			tableName: 'cliente',
-		}
-	);
-	return Cliente;
+  class Cliente extends Model {
+    static associate(models) {
+      Cliente.belongsTo(models.Empresa, {
+        foreignKey: { allowNull: false },
+        onDelete: "RESTRICT",
+        onUpdate: "RESTRICT",
+      });
+      Cliente.hasMany(models.Direccion, {
+        as: "direcciones",
+        foreignKey: { allowNull: false },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      });
+      Cliente.hasMany(models.Orden, {
+        foreignKey: { allowNull: false },
+        onDelete: "RESTRICT",
+        onUpdate: "RESTRICT",
+      });
+      Cliente.hasMany(models.Factura, {
+        foreignKey: { allowNull: false },
+        onDelete: "NO ACTION",
+        onUpdate: "NO ACTION",
+      });
+    }
+  }
+  Cliente.init(
+    {
+      nombre: { type: DataTypes.STRING(30), allowNull: false },
+      apellido: { type: DataTypes.STRING(30), allowNull: false },
+      observaciones: { type: DataTypes.STRING(120) },
+      instagram: { type: DataTypes.STRING(30) },
+      facebook: { type: DataTypes.STRING(30) },
+      celular: { type: DataTypes.STRING(30) },
+      email: { type: DataTypes.STRING(120), allowNull: false },
+      mascota: { type: DataTypes.STRING(30) },
+      tipo: { type: DataTypes.STRING(15), allowNull: false },
+      dni: { type: DataTypes.STRING(15) },
+      razonSocial: { type: DataTypes.STRING(30) },
+      condIva: { type: DataTypes.STRING(30), allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: "Cliente",
+      tableName: "cliente",
+    }
+  );
+  return Cliente;
 };
