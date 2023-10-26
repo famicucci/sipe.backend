ae081efe50f712c9c2fea14730f28a695e1e61c3
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-10-26 11:02:32 -0300

    Get clients with necesary data and pagination

--- app/controllers/ClienteController.js
+++ app/controllers/ClienteController.js
@@ -1,148 +1,196 @@
-const { Cliente, Direccion } = require('../models/index');
+const { Cliente, Direccion } = require("../models/index");
+const { Op } = require("sequelize");
+
+exports.getClients = async (req, res) => {
+  const searchQuery = req.query.search;
+  const page = req.query.page;
+  const pageSize = 50;
+
+  try {
+    const clientes = await Cliente.findAll({
+      attributes: [
+        "id",
+        "nombre",
+        "apellido",
+        "email",
+        "tipo",
+        "razonSocial",
+        "condIva",
+        "observaciones",
+      ],
+      where: {
+        [Op.or]: [
+          { nombre: { [Op.like]: `%${searchQuery}%` } },
+          { apellido: { [Op.like]: `%${searchQuery}%` } },
+          { email: { [Op.like]: `%${searchQuery}%` } },
+          { tipo: { [Op.like]: `%${searchQuery}%` } },
+          { razonSocial: { [Op.like]: `%${searchQuery}%` } },
+          { condIva: { [Op.like]: `%${searchQuery}%` } },
+          { observaciones: { [Op.like]: `%${searchQuery}%` } },
+        ],
+      },
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
+    });
+
+    res.json(clientes);
+  } catch (error) {
+    res.json(error);
+  }
+};
 
 exports.crearCliente = async (req, res) => {
-	try {
-		const cliente = await Cliente.create({
-			nombre: req.body.nombre,
-			apellido: req.body.apellido,
-			observaciones: req.body.observaciones,
-			instagram: req.body.instagram,
-			facebook: req.body.facebook,
-			celular: req.body.celular,
-			email: req.body.email,
-			mascota: req.body.mascota,
-			tipo: req.body.tipo,
-			dni: req.body.dni,
-			razonSocial: req.body.razonSocial,
-			condIva: req.body.condIva,
-			EmpresaId: req.usuarioEmpresaId,
-		});
-		res.status(200).json(cliente);
-	} catch (error) {
-		res.json(error);
-	}
+  try {
+    const cliente = await Cliente.create({
+      nombre: req.body.nombre,
+      apellido: req.body.apellido,
+      observaciones: req.body.observaciones,
+      instagram: req.body.instagram,
+      facebook: req.body.facebook,
+      celular: req.body.celular,
+      email: req.body.email,
+      mascota: req.body.mascota,
+      tipo: req.body.tipo,
+      dni: req.body.dni,
+      razonSocial: req.body.razonSocial,
+      condIva: req.body.condIva,
+      EmpresaId: req.usuarioEmpresaId,
+    });
+    res.status(200).json(cliente);
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 exports.modificarCliente = async (req, res) => {
-	try {
-		const cliente = await Cliente.update(
-			{
-				nombre: req.body.nombre,
-				apellido: req.body.apellido,
-				observaciones: req.body.observaciones,
-				instagram: req.body.instagram,
-				facebook: req.body.facebook,
-				celular: req.body.celular,
-				email: req.body.email,
-				mascota: req.body.mascota,
-				tipo: req.body.tipo,
-				dni: req.body.dni,
-				razonSocial: req.body.razonSocial,
-				condIva: req.body.condIva,
-			},
-			{ where: { id: req.params.Id, EmpresaId: req.usuarioEmpresaId } }
-		);
+  try {
+    const cliente = await Cliente.update(
+      {
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        observaciones: req.body.observaciones,
+        instagram: req.body.instagram,
+        facebook: req.body.facebook,
+        celular: req.body.celular,
+        email: req.body.email,
+        mascota: req.body.mascota,
+        tipo: req.body.tipo,
+        dni: req.body.dni,
+        razonSocial: req.body.razonSocial,
+        condIva: req.body.condIva,
+      },
+      { where: { id: req.params.Id, EmpresaId: req.usuarioEmpresaId } }
+    );
 
-		// verifica si el update fue exitoso
-		if (cliente[0]) {
-			res
-				.status(200)
-				.send({ msg: 'El cliente ha sido modificado!', severity: 'success' });
-		} else {
-			res
-				.status(200)
-				.send({ error: 'No se produjo ningún cambio en la base de datos' });
-		}
-	} catch (error) {
-		res.status(400).send({ error: 'Hubo un error', severity: 'error' });
-	}
+    // verifica si el update fue exitoso
+    if (cliente[0]) {
+      res
+        .status(200)
+        .send({ msg: "El cliente ha sido modificado!", severity: "success" });
+    } else {
+      res
+        .status(200)
+        .send({ error: "No se produjo ningún cambio en la base de datos" });
+    }
+  } catch (error) {
+    res.status(400).send({ error: "Hubo un error", severity: "error" });
+  }
 };
 
 exports.traerClientes = async (req, res) => {
-	try {
-		const clientes = await Cliente.findAll({
-			attributes: [
-				'id',
-				'nombre',
-				'apellido',
-				'observaciones',
-				'instagram',
-				'facebook',
-				'celular',
-				'email',
-				'mascota',
-				'tipo',
-				'dni',
-				'razonSocial',
-				'condIva',
-				'createdAt',
-				'updatedAt',
-			],
-			include: {
-				model: Direccion,
-				as: 'direcciones',
-				attributes: { exclude: ['ClienteId'] },
-			},
-		});
+  const searchQuery = req.query.search;
+  console.log(searchQuery);
+  const page = req.query.page;
+  const pageSize = 50; // Tamaño de la página
 
-		res.json(clientes);
-	} catch (error) {
-		res.json(error);
-	}
+  //   nombre, apellido, email, razon social, nombre local, cond IVA, tipo
+
+  try {
+    const clientes = await Cliente.findAll({
+      attributes: [
+        "id",
+        "nombre",
+        "apellido",
+        // "observaciones",
+        // "instagram",
+        // "facebook",
+        // "celular",
+        "email",
+        // "mascota",
+        "tipo",
+        // "dni",
+        "razonSocial",
+        "condIva",
+        // "createdAt",
+        // "updatedAt",
+      ],
+      //   include: {
+      //     model: Direccion,
+      //     as: "direcciones",
+      //     attributes: { exclude: ["ClienteId"] },
+      //   },
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
+    });
+
+    res.json(clientes);
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 exports.traerCliente = async (req, res) => {
-	console.log(req.params.Email);
-	try {
-		const clientes = await Cliente.findAll({
-			attributes: [
-				'id',
-				'nombre',
-				'apellido',
-				'observaciones',
-				'instagram',
-				'facebook',
-				'celular',
-				'email',
-				'mascota',
-				'tipo',
-				'dni',
-				'razonSocial',
-				'condIva',
-				'createdAt',
-				'updatedAt',
-			],
-			include: {
-				model: Direccion,
-				as: 'direcciones',
-				attributes: { exclude: ['ClienteId'] },
-			},
-			where: { email: req.params.Email },
-		});
+  console.log(req.params.Email);
+  try {
+    const clientes = await Cliente.findAll({
+      attributes: [
+        "id",
+        "nombre",
+        "apellido",
+        "observaciones",
+        "instagram",
+        "facebook",
+        "celular",
+        "email",
+        "mascota",
+        "tipo",
+        "dni",
+        "razonSocial",
+        "condIva",
+        "createdAt",
+        "updatedAt",
+      ],
+      include: {
+        model: Direccion,
+        as: "direcciones",
+        attributes: { exclude: ["ClienteId"] },
+      },
+      where: { email: req.params.Email },
+    });
 
-		res.json(clientes);
-	} catch (error) {
-		res.json(error);
-	}
+    res.json(clientes);
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 exports.eliminarCliente = async (req, res) => {
-	try {
-		const cliente = await Cliente.destroy({
-			where: { id: req.params.Id, EmpresaId: req.usuarioEmpresaId },
-		});
+  try {
+    const cliente = await Cliente.destroy({
+      where: { id: req.params.Id, EmpresaId: req.usuarioEmpresaId },
+    });
 
-		// verifica si el update fue exitoso
-		if (cliente) {
-			res
-				.status(200)
-				.send({ msg: 'El cliente ha sido eliminado!', severity: 'success' });
-		} else {
-			res
-				.status(400)
-				.send({ error: 'No se produjo ningún cambio en la base de datos' });
-		}
-	} catch (error) {
-		res.json(error);
-	}
+    // verifica si el update fue exitoso
+    if (cliente) {
+      res
+        .status(200)
+        .send({ msg: "El cliente ha sido eliminado!", severity: "success" });
+    } else {
+      res
+        .status(400)
+        .send({ error: "No se produjo ningún cambio en la base de datos" });
+    }
+  } catch (error) {
+    res.json(error);
+  }
 };
--- app/routes/api/clientes.js
+++ app/routes/api/clientes.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ClienteController = require('../../controllers/ClienteController');
+const ClienteController = require("../../controllers/ClienteController");
 
-router.get('/', ClienteController.traerClientes);
-router.get('/:Email', ClienteController.traerCliente);
-router.post('/', ClienteController.crearCliente);
-router.put('/:Id', ClienteController.modificarCliente);
-router.delete('/:Id', ClienteController.eliminarCliente);
+router.get("/", ClienteController.getClients);
+router.get("/:Email", ClienteController.traerCliente);
+router.post("/", ClienteController.crearCliente);
+router.put("/:Id", ClienteController.modificarCliente);
+router.delete("/:Id", ClienteController.eliminarCliente);
 
 module.exports = router;
