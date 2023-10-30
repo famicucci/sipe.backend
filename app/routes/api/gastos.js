d19c47fbf58c25b8302702defae22894af80d010
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-10-30 20:30:16 -0300

    get bills

--- app/routes/api/gastos.js
+++ app/routes/api/gastos.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const GastoController = require('../../controllers/GastoController');
+const GastoController = require("../../controllers/GastoController");
 
-router.get('/categorias', GastoController.traerCategorias);
-router.get('/subcategorias', GastoController.traerSubcategorias);
-router.post('/', GastoController.crearGasto);
-router.get('/:Dates', GastoController.traerGastos);
-router.put('/:Id', GastoController.modificarGasto);
-router.delete('/:Id', GastoController.eliminarGasto);
+router.get("/", GastoController.getBills);
+router.get("/categorias", GastoController.traerCategorias);
+router.get("/subcategorias", GastoController.traerSubcategorias);
+router.post("/", GastoController.crearGasto);
+router.get("/:Dates", GastoController.traerGastos);
+router.put("/:Id", GastoController.modificarGasto);
+router.delete("/:Id", GastoController.eliminarGasto);
 
 module.exports = router;
