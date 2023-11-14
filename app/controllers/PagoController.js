0340d64eb4e5fad515a416aa41fcacf2392b646c
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-11-14 11:13:17 -0300

    Get payments of invoice

--- app/controllers/PagoController.js
+++ app/controllers/PagoController.js
@@ -1,102 +1,129 @@
-const { Pago, Factura } = require('../models/index');
-const { sequelize } = require('../models/index');
+const { Pago, Factura, MetodoPago } = require("../models/index");
+const { sequelize } = require("../models/index");
+
+exports.getPayments = async (req, res) => {
+  try {
+    const payments = await Pago.findAll({
+      attributes: [
+        "id",
+        "importe",
+        "estado",
+        "tipo",
+        "createdAt",
+        "MetodoPagoId",
+      ],
+      where: { FacturaId: req.params.invoiceId },
+      include: [
+        {
+          model: MetodoPago,
+          attributes: ["descripcion"],
+        },
+      ],
+      raw: true,
+    });
+
+    res.json(payments);
+  } catch (error) {
+    res.status(400).json({ msg: "There was an error", severity: "error" });
+  }
+};
 
 exports.crearPago = async (req, res) => {
-	try {
-		const pagos = await Pago.create({
-			importe: req.body.importe,
-			FacturaId: req.body.FacturaId,
-			MetodoPagoId: req.body.MetodoPagoId,
-			createdAt: req.body.createdAt,
-			UsuarioId: req.usuarioId,
-			estado: 'v',
-			tipo: 'i',
-		});
+  try {
+    const pagos = await Pago.create({
+      importe: req.body.importe,
+      FacturaId: req.body.FacturaId,
+      MetodoPagoId: req.body.MetodoPagoId,
+      createdAt: req.body.createdAt,
+      UsuarioId: req.usuarioId,
+      estado: "v",
+      tipo: "i",
+    });
 
-		let invoiceTotalAmount;
-		let paymentsTotalAmount = 0;
+    let invoiceTotalAmount;
+    let paymentsTotalAmount = 0;
 
-		// get total amount of current invoice
-		const invoice = await Factura.findByPk(req.body.FacturaId);
-		if (invoice) invoiceTotalAmount = parseFloat(invoice.importeFinal);
+    // get total amount of current invoice
+    const invoice = await Factura.findByPk(req.body.FacturaId);
+    if (invoice) invoiceTotalAmount = parseFloat(invoice.importeFinal);
 
-		// get all payments of current invoice
-		const payments = await Pago.findAll({
-			where: { FacturaId: req.body.FacturaId },
-		});
+    // get all payments of current invoice
+    const payments = await Pago.findAll({
+      where: { FacturaId: req.body.FacturaId },
+    });
 
-		if (payments) {
-			payments.forEach((x) => {
-				paymentsTotalAmount += parseFloat(x.importe);
-			});
-		}
+    if (payments) {
+      payments.forEach((x) => {
+        paymentsTotalAmount += parseFloat(x.importe);
+      });
+    }
 
-		//  result
-		const result = invoiceTotalAmount - paymentsTotalAmount;
+    //  result
+    const result = invoiceTotalAmount - paymentsTotalAmount;
 
-		if (result === 0)
-			await Factura.update(
-				{
-					estadoPago: 'Pago',
-				},
-				{ where: { id: req.body.FacturaId } }
-			);
+    if (result === 0)
+      await Factura.update(
+        {
+          estadoPago: "Pago",
+        },
+        { where: { id: req.body.FacturaId } }
+      );
 
-		res.json(pagos);
-	} catch (error) {
-		res.status(400).json({ msg: 'There was an error', severity: 'error' });
-	}
+    res.json(pagos);
+  } catch (error) {
+    res.status(400).json({ msg: "There was an error", severity: "error" });
+  }
 };
 
 exports.cancelPayment = async (req, res) => {
-	const t = await sequelize.transaction();
+  const t = await sequelize.transaction();
 
-	try {
-		// select payment to be canceled
-		const payment = await Pago.findByPk(req.params.Id);
+  try {
+    // select payment to be canceled
+    const payment = await Pago.findByPk(req.params.Id);
 
-		if (payment.estado === 'c')
-			res
-				.status(400)
-				.json({ msg: 'Payment was already canceled', severity: 'error' });
+    if (payment.estado === "c")
+      res
+        .status(400)
+        .json({ msg: "Payment was already canceled", severity: "error" });
 
-		if (payment) {
-			const negativePayment = await Pago.create(
-				{
-					importe: -payment.importe,
-					FacturaId: payment.FacturaId,
-					MetodoPagoId: req.body.MetodoPagoId,
-					UsuarioId: req.usuarioId,
-					estado: 'v',
-					tipo: 'nc',
-				},
-				{
-					transaction: t,
-				}
-			);
+    if (payment) {
+      const negativePayment = await Pago.create(
+        {
+          importe: -payment.importe,
+          FacturaId: payment.FacturaId,
+          MetodoPagoId: req.body.MetodoPagoId,
+          UsuarioId: req.usuarioId,
+          estado: "v",
+          tipo: "nc",
+        },
+        {
+          transaction: t,
+        }
+      );
 
-			// update estadoPago in invoice with: 'Pendiente'
-			await Factura.update(
-				{
-					estadoPago: 'Pendiente',
-				},
-				{ transaction: t, where: { id: payment.FacturaId } }
-			);
+      // update estadoPago in invoice with: 'Pendiente'
+      await Factura.update(
+        {
+          estadoPago: "Pendiente",
+        },
+        { transaction: t, where: { id: payment.FacturaId } }
+      );
 
-			await Pago.update(
-				{
-					estado: 'c',
-				},
-				{ transaction: t, where: { id: req.params.Id } }
-			);
+      await Pago.update(
+        {
+          estado: "c",
+        },
+        { transaction: t, where: { id: req.params.Id } }
+      );
 
-			await t.commit();
-			res.status(200).json(negativePayment);
-		} else {
-			await t.rollback();
-			res.status(400).json({ msg: 'Payment doesn´t exist', severity: 'error' });
-		}
-	} catch (error) {
-		res.status(400).json({ msg: 'There was an error', severity: 'error' });
-	}
+      await t.commit();
+      res.status(200).json(negativePayment);
+    } else {
+      await t.rollback();
+      res.status(400).json({ msg: "Payment doesn´t exist", severity: "error" });
+    }
+  } catch (error) {
+    res.status(400).json({ msg: "There was an error", severity: "error" });
+  }
 };
--- app/routes/api/pagos.js
+++ app/routes/api/pagos.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const PagoController = require('../../controllers/PagoController');
+const PagoController = require("../../controllers/PagoController");
 
-router.post('/', PagoController.crearPago);
-router.patch('/:Id', PagoController.cancelPayment);
+router.get("/:invoiceId", PagoController.getPayments);
+router.post("/", PagoController.crearPago);
+router.patch("/:Id", PagoController.cancelPayment);
 
 module.exports = router;
