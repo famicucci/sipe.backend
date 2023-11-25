6bb44e6effcc9ac978b1d9b1d7536ea901b92ac9
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-11-25 17:27:35 -0300

    Get products with quantity and prices

--- app/routes/api/productos.js
+++ app/routes/api/productos.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ProductoController = require('../../controllers/ProductoController');
+const ProductoController = require("../../controllers/ProductoController");
 
-router.get('/', ProductoController.traerProductos);
-router.post('/', ProductoController.crearProducto);
-router.put('/:ProductoCodigo', ProductoController.modificarProducto);
-router.delete('/:ProductoCodigo', ProductoController.eliminarProducto);
-router.get('/a-mover', ProductoController.traerProductosAMover);
+router.get("/", ProductoController.traerProductos);
+router.get(
+  "/:stockPointId/:priceList",
+  ProductoController.getProductsWithStockAndPrice
+);
+router.post("/", ProductoController.crearProducto);
+router.put("/:ProductoCodigo", ProductoController.modificarProducto);
+router.delete("/:ProductoCodigo", ProductoController.eliminarProducto);
+router.get("/a-mover", ProductoController.traerProductosAMover);
 
 module.exports = router;
