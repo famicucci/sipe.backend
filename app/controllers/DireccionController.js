504636255388d402aaff0af92590cd5a20145601
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-12-01 17:48:33 -0300

    Get adresses of client

--- app/controllers/DireccionController.js
+++ app/controllers/DireccionController.js
@@ -1,30 +1,45 @@
-const { Direccion } = require('../models/index');
+const { Direccion } = require("../models/index");
+
+exports.getAdresses = async (req, res) => {
+  try {
+    const direccion = await Direccion.findAll(
+      {
+        where: { ClienteId: req.params.clientId },
+      },
+      { raw: true }
+    );
+    res.status(200).json(direccion);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+};
 
 exports.crearDireccion = async (req, res) => {
-	try {
-		const direccion = await Direccion.create({
-			calle: req.body.calle,
-			numeroCalle: req.body.numeroCalle,
-			piso: req.body.piso,
-			depto: req.body.depto,
-			barrio: req.body.barrio,
-			ciudad: req.body.ciudad,
-			provincia: req.body.provincia,
-			codPostal: req.body.codPostal,
-			refDireccion: req.body.refDireccion,
-			ClienteId: req.body.ClienteId,
-		});
-		res.status(200).json(direccion);
-	} catch (error) {
-		res.status(400).json(error);
-	}
+  try {
+    const direccion = await Direccion.create({
+      calle: req.body.calle,
+      numeroCalle: req.body.numeroCalle,
+      piso: req.body.piso,
+      depto: req.body.depto,
+      barrio: req.body.barrio,
+      ciudad: req.body.ciudad,
+      provincia: req.body.provincia,
+      codPostal: req.body.codPostal,
+      refDireccion: req.body.refDireccion,
+      ClienteId: req.body.ClienteId,
+    });
+    res.status(200).json(direccion);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 exports.deleteAdress = async (req, res) => {
-	try {
-		await Direccion.destroy({ where: { id: JSON.parse(req.params.Ids) } });
-		res.status(200).json({ msg: 'Adresses were deleted' });
-	} catch (error) {
-		res.status(400).json(error);
-	}
+  try {
+    await Direccion.destroy({ where: { id: JSON.parse(req.params.Ids) } });
+    res.status(200).json({ msg: "Adresses were deleted" });
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
--- app/routes/api/direcciones.js
+++ app/routes/api/direcciones.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const DireccionController = require('../../controllers/DireccionController');
+const DireccionController = require("../../controllers/DireccionController");
 
-router.post('/', DireccionController.crearDireccion);
-router.delete('/:Ids', DireccionController.deleteAdress);
+router.get("/:clientId", DireccionController.getAdresses);
+router.post("/", DireccionController.crearDireccion);
+router.delete("/:Ids", DireccionController.deleteAdress);
 
 module.exports = router;
