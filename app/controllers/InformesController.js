cb995c25e734cef83a482b86852a8e827f50ee3a
Micucci <B062493@servexternos.santandertecnologia.com.ar>
2023-12-14 23:14:53 -0300

    Add Categories to the expenses by month

--- app/controllers/InformesController.js
+++ app/controllers/InformesController.js
@@ -1,188 +1,205 @@
 const {
-	Pago,
-	Gasto,
-	Factura,
-	FacturaDetalle,
-	Producto,
-} = require('../models/index');
-const { sequelize } = require('../models/index');
-const { Op, literal } = require('sequelize');
-const moment = require('moment');
+  Pago,
+  Gasto,
+  Factura,
+  FacturaDetalle,
+  Producto,
+  GastoCategoria,
+} = require("../models/index");
+const { sequelize } = require("../models/index");
+const { Op, literal } = require("sequelize");
+const moment = require("moment");
 
 exports.getIncomesByMonths = async (req, res) => {
-	const startDate = moment(req.query.startDate)
-	const endDate = moment(req.query.endDate).add({
-		hours: 24,
-	});
+  const startDate = moment(req.query.startDate);
+  const endDate = moment(req.query.endDate).add({
+    hours: 24,
+  });
 
-	try {
-		const ingresosBrutos = await Pago.findAll({
-			attributes: [
-				[sequelize.fn('sum', sequelize.col('importe')), 'totalIncome'],
-				[sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')"), 'monthYear']
+  try {
+    const ingresosBrutos = await Pago.findAll({
+      attributes: [
+        [sequelize.fn("sum", sequelize.col("importe")), "totalIncome"],
+        [sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')"), "monthYear"],
+      ],
+      where: {
+        createdAt: {
+          [Op.between]: [startDate, endDate],
+        },
+      },
+      group: [sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')")],
+      order: [sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')")],
+    });
 
-			],
-			where: {
-				createdAt: {
-					[Op.between]: [startDate, endDate],
-				},
-			},
-			group: [
-				sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')")
-			  ],
-			order: [
-				sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')")
-			  ]
-		});
-
-		res.status(200).json(ingresosBrutos);
-	} catch (error) {
-		res.json(error);
-	}
+    res.status(200).json(ingresosBrutos);
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 exports.getExpensesByMonths = async (req, res) => {
-	const startDate = moment(req.query.startDate)
-	const endDate = moment(req.query.endDate).add({
-		hours: 24,
-	});
+  const startDate = moment(req.query.startDate);
+  const endDate = moment(req.query.endDate).add({
+    hours: 24,
+  });
+  const byCategory = req.query.byCategory;
 
-	try {
-		const expenses = await Gasto.findAll({
-			attributes: [
-				[sequelize.fn('sum', sequelize.col('importe')), 'totalExpenses'],
-				[sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')"), 'monthYear']
+  const attributes =
+    byCategory === "true"
+      ? [
+          [sequelize.fn("sum", sequelize.col("importe")), "totalExpenses"],
+          [sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')"), "monthYear"],
+          "GastoCategoriaId",
+        ]
+      : [
+          [sequelize.fn("sum", sequelize.col("importe")), "totalExpenses"],
+          [sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')"), "monthYear"],
+        ];
 
-			],
-			where: {
-				createdAt: {
-					[Op.between]: [startDate, endDate],
-				},
-			},
-			group: [
-				sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')")
-			  ],
-			order: [
-				sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')")
-			  ]
-		});
+  const group =
+    byCategory === "true"
+      ? [
+          sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')"),
+          "GastoCategoriaId",
+        ]
+      : [sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')")];
 
-		res.status(200).json(expenses);
-	} catch (error) {
-		res.status(400).json(expenses);
-	}
+  try {
+    const expenses = await Gasto.findAll({
+      attributes: attributes,
+      where: {
+        createdAt: {
+          [Op.between]: [startDate, endDate],
+        },
+      },
+      group: group,
+      include: byCategory === "true" ? [
+        {
+          model: GastoCategoria,
+          as: "GastoCategoria",
+          attributes: ["descripcion"],
+        },
+      ] : [],
+      order: [sequelize.literal("DATE_FORMAT(createdAt, '%m-%Y')")],
+      raw: true,
+    });
+
+    res.status(200).json(expenses);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 exports.traerIngresosBrutos = async (req, res) => {
-	// desde debe tomar el dia ingresado a las 00:00:00
-	// hasta debe tomar el dia ingresado a las 24:00:00
-	const startDate = moment(req.query.startDate).subtract({
-		hours: 3,
-	});
-	const endDate = moment(req.query.endDate).add({
-		hours: 21,
-	});
-	try {
-		const ingresosBrutos = await Pago.findAll({
-			attributes: [[sequelize.fn('sum', sequelize.col('importe')), 'importe']],
-			where: {
-				createdAt: {
-					[Op.between]: [startDate, endDate],
-				},
-			},
-		});
+  // desde debe tomar el dia ingresado a las 00:00:00
+  // hasta debe tomar el dia ingresado a las 24:00:00
+  const startDate = moment(req.query.startDate).subtract({
+    hours: 3,
+  });
+  const endDate = moment(req.query.endDate).add({
+    hours: 21,
+  });
+  try {
+    const ingresosBrutos = await Pago.findAll({
+      attributes: [[sequelize.fn("sum", sequelize.col("importe")), "importe"]],
+      where: {
+        createdAt: {
+          [Op.between]: [startDate, endDate],
+        },
+      },
+    });
 
-		if (ingresosBrutos[0].importe) {
-			res.status(200).json(ingresosBrutos[0].importe);
-		} else {
-			res.status(200).json('0');
-		}
-	} catch (error) {
-		res.json(error);
-	}
+    if (ingresosBrutos[0].importe) {
+      res.status(200).json(ingresosBrutos[0].importe);
+    } else {
+      res.status(200).json("0");
+    }
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 exports.traerGastos = async (req, res) => {
-	const startDate = moment(req.body.startDate).subtract({
-		hours: 3,
-	});
-	const endDate = moment(req.body.endDate).add({
-		hours: 21,
-	});
+  const startDate = moment(req.body.startDate).subtract({
+    hours: 3,
+  });
+  const endDate = moment(req.body.endDate).add({
+    hours: 21,
+  });
 
-	let whereClausula = {
-		createdAt: {
-			[Op.between]: [startDate, endDate],
-		},
-	};
+  let whereClausula = {
+    createdAt: {
+      [Op.between]: [startDate, endDate],
+    },
+  };
 
-	if (req.body.IdCategoria) {
-		whereClausula = {
-			...whereClausula,
-			GastoCategoriaId: req.body.IdCategoria,
-		};
-		if (req.body.IdSubcategoria) {
-			whereClausula = {
-				...whereClausula,
-				GastoSubcategoriaId: req.body.IdSubcategoria,
-			};
-		}
-	}
+  if (req.body.IdCategoria) {
+    whereClausula = {
+      ...whereClausula,
+      GastoCategoriaId: req.body.IdCategoria,
+    };
+    if (req.body.IdSubcategoria) {
+      whereClausula = {
+        ...whereClausula,
+        GastoSubcategoriaId: req.body.IdSubcategoria,
+      };
+    }
+  }
 
-	try {
-		const gastos = await Gasto.findAll({
-			attributes: [[sequelize.fn('sum', sequelize.col('importe')), 'importe']],
-			where: whereClausula,
-		});
+  try {
+    const gastos = await Gasto.findAll({
+      attributes: [[sequelize.fn("sum", sequelize.col("importe")), "importe"]],
+      where: whereClausula,
+    });
 
-		if (gastos[0].importe) {
-			res.status(200).json(gastos[0].importe);
-		} else {
-			res.status(200).json('0');
-		}
-	} catch (error) {
-		res.json(error);
-	}
+    if (gastos[0].importe) {
+      res.status(200).json(gastos[0].importe);
+    } else {
+      res.status(200).json("0");
+    }
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 // traer los productos con mayor facturación y la cantidad que se vendieron
 exports.traerProductosVendidos = async (req, res) => {
-	const startDate = moment(req.body.startDate).subtract({
-		hours: 3,
-	});
-	const endDate = moment(req.body.endDate).add({
-		hours: 21,
-	});
+  const startDate = moment(req.body.startDate).subtract({
+    hours: 3,
+  });
+  const endDate = moment(req.body.endDate).add({
+    hours: 21,
+  });
 
-	let whereClausula = {
-		createdAt: {
-			[Op.between]: [startDate, endDate],
-		},
-		estado: 'v',
-	};
+  let whereClausula = {
+    createdAt: {
+      [Op.between]: [startDate, endDate],
+    },
+    estado: "v",
+  };
 
-	try {
-		const productos = await FacturaDetalle.findAll({
-			attributes: [
-				'ProductoCodigo',
-				[sequelize.fn('sum', sequelize.col('cantidad')), 'cantidad'],
-				[sequelize.literal('SUM(cantidad * pu)'), 'totalFacturado'],
-			],
+  try {
+    const productos = await FacturaDetalle.findAll({
+      attributes: [
+        "ProductoCodigo",
+        [sequelize.fn("sum", sequelize.col("cantidad")), "cantidad"],
+        [sequelize.literal("SUM(cantidad * pu)"), "totalFacturado"],
+      ],
 
-			include: [
-				{ model: Producto, attributes: ['descripcion'] },
-				{
-					model: Factura,
-					attributes: ['createdAt'],
-					where: whereClausula,
-				},
-			],
-			group: ['ProductoCodigo'],
-			raw: true,
-		});
+      include: [
+        { model: Producto, attributes: ["descripcion"] },
+        {
+          model: Factura,
+          attributes: ["createdAt"],
+          where: whereClausula,
+        },
+      ],
+      group: ["ProductoCodigo"],
+      raw: true,
+    });
 
-		res.status(200).json(productos);
-	} catch (error) {
-		res.json(error);
-	}
+    res.status(200).json(productos);
+  } catch (error) {
+    res.json(error);
+  }
 };
