267e60ab46f3c3e3a4a4e8da1425cfb222636c51
famicucci <fmicucci@servexternos.santandertecnologia.com.ar>
2024-01-31 14:50:46 -0300

    Create and delete lists

--- app/controllers/ListaPrecioController.js
+++ app/controllers/ListaPrecioController.js
@@ -12,3 +12,30 @@ exports.getPriceLists = async (req, res) => {
     res.status(400).send(error);
   }
 };
+
+exports.createPriceList = async (req, res) => {
+  try {
+    const precios = await ListaPrecio.create({
+      descripcion: req.body.descripcion,
+      estado: req.body.estado,
+      EmpresaId: req.usuarioEmpresaId,
+    });
+
+    res.status(200).json(precios);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+exports.deletePriceList = async (req, res) => {
+  try {
+    await ListaPrecio.destroy({
+      where: { id: req.params.id },
+    });
+
+    res.status(200).json("price list deleted");
+  } catch (error) {
+    res.statusMessage = "error deleting price list";
+    return res.status(400).end();
+  }
+};
--- app/routes/api/listas-precio.js
+++ app/routes/api/listas-precio.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const ListaPrecioController = require("../../controllers/ListaPrecioController");
 
 router.get("/", ListaPrecioController.getPriceLists);
+router.post("/", ListaPrecioController.createPriceList);
+router.delete("/:id", ListaPrecioController.deletePriceList);
 
 module.exports = router;
