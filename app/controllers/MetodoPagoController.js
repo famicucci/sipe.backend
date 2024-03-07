54a546198171d7f90e1023597ce4ebd2348cc1fc
famicucci <fmicucci@servexternos.santandertecnologia.com.ar>
2024-03-07 17:33:30 -0300

    Create payment method

--- app/controllers/MetodoPagoController.js
+++ app/controllers/MetodoPagoController.js
@@ -1,13 +1,26 @@
-const { MetodoPago } = require('../models/index');
+const { MetodoPago } = require("../models/index");
 
 exports.traerMetodosPago = async (req, res) => {
-	try {
-		const metodosPago = await MetodoPago.findAll({
-			attributes: ['id', 'descripcion'],
-			where: { EmpresaId: req.usuarioEmpresaId },
-		});
-		res.status(200).json(metodosPago);
-	} catch (error) {
-		res.json(error);
-	}
+  try {
+    const metodosPago = await MetodoPago.findAll({
+      attributes: ["id", "descripcion"],
+      where: { EmpresaId: req.usuarioEmpresaId },
+    });
+    res.status(200).json(metodosPago);
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+exports.createPaymentMethod = async (req, res) => {
+  try {
+    const metodosPago = await MetodoPago.create({
+      descripcion: req.body.descripcion,
+      EmpresaId: req.usuarioEmpresaId,
+    });
+    res.status(200).json(metodosPago);
+  } catch (error) {
+    res.statusMessage = `Hubo un error`;
+    return res.status(400).end();
+  }
 };
--- app/routes/api/metodospago.js
+++ app/routes/api/metodospago.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const MetodoPagoController = require('../../controllers/MetodoPagoController');
+const MetodoPagoController = require("../../controllers/MetodoPagoController");
 
-router.get('/', MetodoPagoController.traerMetodosPago);
+router.get("/", MetodoPagoController.traerMetodosPago);
+router.post("/", MetodoPagoController.createPaymentMethod);
 
 module.exports = router;
