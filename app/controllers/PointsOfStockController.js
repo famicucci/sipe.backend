d1cd7a6c5a648529b0595c20cc2bbf85733a3c03
famicucci <fmicucci@servexternos.santandertecnologia.com.ar>
2024-03-12 09:02:58 -0300

    Create and delete stock of point

--- app/controllers/PointsOfStockController.js
+++ app/controllers/PointsOfStockController.js
@@ -13,3 +13,28 @@ exports.getPointsOfStock = async (req, res) => {
     res.json(error);
   }
 };
+
+exports.createPointOfStock = async (req, res) => {
+  try {
+    const status = await PtoStock.create({
+      descripcion: req.body.descripcion,
+      EmpresaId: req.usuarioEmpresaId,
+    });
+    res.status(200).json(status);
+  } catch (error) {
+    res.statusMessage = `Hubo un error`;
+    return res.status(400).end();
+  }
+};
+
+exports.deleteStockOfPoint = async (req, res) => {
+  try {
+    await PtoStock.destroy({
+      where: { id: req.params.id },
+    });
+    res.status(200).json("stock of point deleted");
+  } catch (error) {
+    res.statusMessage = `Hubo un error`;
+    return res.status(400).end();
+  }
+};
--- app/routes/api/ptos-stock.js
+++ app/routes/api/ptos-stock.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const PointsOfStockController = require("../../controllers/PointsOfStockController");
 
 router.get("/", PointsOfStockController.getPointsOfStock);
+router.post("/", PointsOfStockController.createPointOfStock);
+router.delete("/:id", PointsOfStockController.deleteStockOfPoint);
 
 module.exports = router;
