c4fa897750798f59b46a629d3d8ed4d55572488b
Francisco A. Micucci <francisco.micucci@diveria.com>
2025-06-16 10:08:45 -0300

    change endpoint route from get to post

--- app/routes/api/sales-inventory.js
+++ app/routes/api/sales-inventory.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const SalesInventoryController = require("../../controllers/SalesInventoryController");
 
-router.get("/:stockPointId?", SalesInventoryController.getSalesInventory);
+router.post("/:stockPointId?", SalesInventoryController.getSalesInventory);
 
 module.exports = router;
