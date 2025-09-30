b5d09bece29764f702fb6c11b101d87c8fcd55a8
Francisco A. Micucci <francisco.micucci@diveria.com>
2025-09-29 21:47:24 -0300

    create logs for env variables

--- app/server.js
+++ app/server.js
@@ -21,8 +21,14 @@ app.use("/api", apiRouter);
 
 // arrancar el servidor
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`La aplicación ha arrancado en el puerto: ${PORT}`);
+  console.log("Conectando a la base de datos...");
+  console.log(`Host de la base de datos: ${process.env.DB_HOST}`);
+  console.log(`Base de datos: ${process.env.DB_DATABASE}`);
+  console.log(`Usuario de la base de datos: ${process.env.DB_USERNAME}`);
+  console.log(`Contraseña de la base de datos: ${process.env.DB_PASSWORD}`);
+  console.log(`Dialect de la base de datos: ${process.env.DB_DIALECT}`);
 
   // sequelize.sync({ force: false }).then(() => {
   // 	console.log('Se ha establecido la conexión');
