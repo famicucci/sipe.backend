54e6012e303faea3ec6a9841bf1f4ce85226d16b
Francisco A. Micucci <francisco.micucci@diveria.com>
2025-11-22 15:35:43 -0300

    create migrations for configurations

---
+++ app/database/migrations/20251122182413-create-configurations.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("configurations", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "boolean", // 'boolean', 'string', 'number', etc.
+      },
+      value: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("configurations");
+  },
+};
